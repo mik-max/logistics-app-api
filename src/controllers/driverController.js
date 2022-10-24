00e20bf475557e019d1b59d428627f7f24b027e3
Michael Chinye <michaelchinye2018@gmail.com>
2022-10-24 12:00:46 +0100

    added email validation for user signup

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -1,12 +1,17 @@
 import { createDriverData, updateDriverData } from "../data/drivers/index.js";
-
+import { validateEmail } from "../utilities/emailValidation.js";
 
 
 const createDriver = async (req, res) => {
      try {
-          const data = req.body;
-          const create = await createDriverData(data);
-          res.status(200).send(create);
+          let validatedEmail = validateEmail(req.body.email)
+          if(validatedEmail){
+               const data = req.body;
+               await createDriverData(data);
+             res.status(200).send({status:'Ok', data:null, message: "User has been successfully created"});
+          }else{
+               res.status(400).send({status:'Failed', data:null, message: "Email is invalid"});
+          }  
      } catch (error) {
           res.status(400).send(error.message);
           console.log(error.message);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,15 @@
 import { createUserData } from "../data/customer/index.js";
-
+import { validateEmail } from "../utilities/emailValidation.js";
 const createUser =  async (req, res) => {
     try {
-        const data = req.body;
-        const create = await createUserData(data);
-        res.status(200).send(create);
+     let validatedEmail = validateEmail(req.body.email)
+     if(validatedEmail){
+          const data = req.body;
+          await createUserData(data);
+        res.status(200).send({status:'Ok', data:null, message: "User has been successfully created"});
+     }else{
+          res.status(400).send({status:'Failed', data:null, message: "Email is invalid"});
+     }  
     } catch (error) {
         res.status(400).send(error.message)
         console.log(error.message)
